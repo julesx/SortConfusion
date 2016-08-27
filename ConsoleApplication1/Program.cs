65121323a1c500c9b40258bfc1bd89de5343f636
Julien <Julien@JR-NEW-PC>
2016-08-27 17:06:18 -0400

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DynamicData;
+using DynamicData.Binding;
+using DynamicData.Controllers;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static IObservableCollection<string> Items { get; set; } = new ObservableCollectionExtended<string>();
+
+        static void Main(string[] args)
+        {
+            var ItemCache = new SourceCache<string, string>(x => x);
+            var ItemsSorter = new ItemSorter();
+            var PageController = new PageController();
+            var FilterController = new FilterController<string>(FilterFunc);
+            var SortController = new SortController<string>(ItemsSorter);
+
+            var FilteredCache = ItemCache.Connect()
+                .Filter(FilterController)
+                .AsObservableCache();
+
+            var Connected = FilteredCache
+                .Connect()
+                .Sort(SortController)
+                //.ObserveOnDispatcher()
+                .Bind(Items)
+                .Subscribe();
+
+            ItemCache.AddOrUpdate("wtf");
+
+            Console.ReadLine();
+        }
+
+        public static bool FilterFunc(string x)
+        {
+            return true;
+        }
+    }
+
+    public class ItemSorter : IComparer<String>
+    {
+        public int Compare(string x, string y)
+        {
+            Console.WriteLine("comparing " + x + " to " + y);
+            return x.CompareTo(y);
+        }
+    }
+}
